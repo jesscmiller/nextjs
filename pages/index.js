342ddfdc0ea9750e579cac68746be3e2d1f3306f
jesscmiller <92549019+jesscmiller@users.noreply.github.com>
2021-10-14 21:45:07 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Jessica&apos;s website!</a>
+          Welcome to Jessica&apos;s website!
         </h1>
 
         <p className={styles.description}>
