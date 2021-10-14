11f4932a5938982c3abfc87f88543da3ed72fc5e
jesscmiller <92549019+jesscmiller@users.noreply.github.com>
2021-10-14 22:02:51 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,23 +27,21 @@ export default function Home() {
             <h2>Instagram &rarr;</h2>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://www.twitter.com/jessicacmiller_" className={styles.card}>
             <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+          <a>
+            href="https://www.materialityexposed.com"
             className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          
+            <h2>Materiality Exposed exhibition &rarr;</h2>
           </a>
 
-          <a
+          <a>
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
-          >
+          
             <h2>Deploy &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
