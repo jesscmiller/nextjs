c7beb07d598034400c68082787c24d9ce68cf29d
jesscmiller <92549019+jesscmiller@users.noreply.github.com>
2021-11-28 16:59:01 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
+import Navbar from '../comps/Navbar'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
    <div>
     <h1>Homepage</h1>
+    <Navbar />
    </div>
 
       <main className={styles.main}>
