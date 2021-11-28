ec8eaacdfc19091ba23f05b1d9a469d2ed943a8c
jesscmiller <92549019+jesscmiller@users.noreply.github.com>
2021-11-28 17:04:47 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Navbar from '../comps/Navbar'
+import Navbar from '../comps/Footer'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
@@ -7,8 +8,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
    <div>
-    <h1>Homepage</h1>
     <Navbar />
+    <h1>Homepage</h1>
+    <p>Hello</p>
+    <Footer />
    </div>
 
       <main className={styles.main}>
@@ -38,18 +41,6 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+  
   )
 }
