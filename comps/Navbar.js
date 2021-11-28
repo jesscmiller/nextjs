233b370a3bfc8caf6b557f8b4b9ab40dde01c856
jesscmiller <92549019+jesscmiller@users.noreply.github.com>
2021-11-28 16:57:39 +0000

    Create Navbar.js

---
+++ comps/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Jessica Miller</h1>
+      </div>
+      <a>Home</a>
+      <a>About</a>
+      <a>Blog</a>
+      <a>Gallery</a>
+      <a>Contact</a>
+    </nav>
+  );
+}
+
+export default Navbar; 
