070e5f4e1b04403c20119e5864e01bc55afa36e0
jesscmiller <92549019+jesscmiller@users.noreply.github.com>
2021-12-03 23:23:34 +0000

    Update index.js

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,1 +1,12 @@
+const Blog = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+    </div>
+  );
+}
 
+export default Blog; 
