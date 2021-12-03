94e88142cb0c4544338d88fd92ce263779bf39f3
jesscmiller <92549019+jesscmiller@users.noreply.github.com>
2021-12-03 23:24:46 +0000

    Update index.js

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,1 +1,12 @@
+const Gallery = () => {
+  return (
+    <div>
+      <h1>Gallery</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
+    </div>
+  );
+}
 
+export default Gallery; 
