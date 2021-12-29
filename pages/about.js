328662564b76461174c6093c8095b9a838ad119f
jesscmiller <jessicacmillerphotography@gmail.com>
2021-12-29 12:50:23 +0000

    added gitpod support

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <h1>About Me</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
