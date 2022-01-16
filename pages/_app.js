31095b02b2b8a62dd374eaada93b10923ed4fd9a
Douglas Willetts <douglasa.willetts@googlemail.com>
2022-01-16 12:26:05 +0000

    made navbar global

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Navbar from '../comps/Navbar.js'
+import Footer from '../comps/Footer.js'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar/>
+      <Component {...pageProps} />
+      <Footer/>
+    </>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Navbar from '../comps/Navbar'
-import Footer from '../comps/Footer'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
@@ -8,10 +6,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Welcome to Jessica&apos;s website!</h1>
       <p>My new site is currently under construction so please bear with me.</p>
-      <Footer />
     </div>
   )
 }
