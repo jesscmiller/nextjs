c5849a7a00d31dfc4f242215eeb09c4b8327d20b
jesscmiller <92549019+jesscmiller@users.noreply.github.com>
2022-01-17 21:47:33 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Jessica&apos;s website!</h1>
-      <p>My new site is currently under construction so please bear with me.</p>
+      <h1 className={styles.title}>Welcome to Jessica&apos;s website!</h1>
+      <p className={styles.text}>My new site is currently under construction so please bear with me.</p>
     </div>
   )
 }
