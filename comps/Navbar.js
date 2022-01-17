62ce31dbccbf8b7e77f32318e8ff1410a077dbd0
jesscmiller <92549019+jesscmiller@users.noreply.github.com>
2022-01-17 22:22:11 +0000

    Update Navbar.js

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -3,9 +3,6 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav>
-      <div className="logo">
-        <h1>Jessica Miller</h1>
-      </div>
       <Link href="/"><a>Home</a></Link>
       <Link href="/about"><a>About</a></Link>
       <Link href="/blog"><a>Blog</a></Link>
