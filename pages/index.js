2a24f332fd045f673ca4f323239f9a47d1543f60
jesscmiller <92549019+jesscmiller@users.noreply.github.com>
2022-01-19 20:41:44 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h1 className={styles.title}>Welcome to Jessica&apos;s website!</h1>
+      <p className={styles.title}>Welcome!</p>
       <p className={styles.text}>My new site is currently under construction so please bear with me.</p>
       <Link href={'https://www.instagram.com/jessc.miller/'}> 
         <a className={styles.btn}>Follow me on Instagram</a>
