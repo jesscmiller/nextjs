e864dbfb146d6f196523d250923e55a3b91e5ced
jesscmiller <jessicacmillerphotography@gmail.com>
2022-01-26 21:37:52 +0000

    added link to header text back to home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import Navbar from '../comps/Navbar.js'
 import Footer from '../comps/Footer.js'
+import Link from 'next/link';
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <div className="mainheader"> <p>JESSICA MILLER</p> </div>
+      <div className="mainheader"><Link href="/"><a><p>JESSICA MILLER</p></a></Link> </div>
       <div className="subheader"> <p>PHOTOGRAPHER</p> </div>
       <Navbar/>
       <Component {...pageProps} />
