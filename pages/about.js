24958bbddac17f0b2082ddc546f4f14bf53f94dd
jesscmiller <jessicacmillerphotography@gmail.com>
2022-01-31 20:57:50 +0000

    add pic

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,11 @@
+import Image from "next/image";
 const About = () => {
   return (
     <div>
       <h1>About Me</h1>
+      <div className="profile">
+        <Image src="/web1Jess2.jpg" width={128} height={128} />
+      </div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro reiciendis ea quibusdam nemo nobis hic iure quos odio corporis, laudantium animi laborum voluptatem quia, veniam fuga aperiam officia sequi!</p>
