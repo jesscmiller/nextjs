f6ec9559392dfefac52b419e4cda71bca3ffb015
jesscmiller <jessicacmillerphotography@gmail.com>
2022-02-10 20:51:51 +0000

    adding to nav bar

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -4,10 +4,11 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav>
-      <Link href="/"><a>Home</a></Link>
       <Link href="/about"><a>About</a></Link>
       <Link href="/blog"><a>Blog</a></Link>
       <Link href="/gallery"><a>Gallery</a></Link>
+      <Link href="/articles"><a>Articles</a></Link>
+      <Link href="/exhibitions"><a>Exhibitions</a></Link>
       <Link href="/contact"><a>Contact</a></Link>
     </nav>
   );
