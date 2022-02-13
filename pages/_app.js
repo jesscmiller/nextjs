ba9bbf3aea1a64c62abe17fe7390b122d81abdb1
Douglas Willetts <douglasa.willetts@googlemail.com>
2022-02-13 11:33:35 +0000

    Instagram Item sorted.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
-import Navbar from '../comps/Navbar.js'
-import Footer from '../comps/Footer.js'
+import Navbar from '../comps/Navbar.js';
+import Footer from '../comps/Footer.js';
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
       <Footer/>
       <div className= "social"> 
-        <Link href="https://www.instagram.com/jessc.miller/"><a><Image src='/instagram.ico'  height={26} width={26}/></a></Link>  <Link href="https://www.twitter.com/jessicacmiller_"><a><Image src='/twitter.ico'  height={22.5} width={22.5}/></a></Link> <Link href="https://www.linkedin.com/in/jessica-cmiller/"><a><Image src='/linkedin.ico'  height={22.5} width={22.5}/></a></Link>
+        <Link href="https://www.instagram.com/jessc.miller/"><a><Image src='/instagram.ico'  height={22.5} width={22.5}/></a></Link>  <Link href="https://www.twitter.com/jessicacmiller_"><a><Image src='/twitter.ico'  height={22.5} width={22.5}/></a></Link> <Link href="https://www.linkedin.com/in/jessica-cmiller/"><a><Image src='/linkedin.ico'  height={22.5} width={22.5}/></a></Link>
       </div>  
     </>
   );
