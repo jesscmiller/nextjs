d5f4670e68ff873954faaae777ebcee8a726d13a
jesscmiller <jessicacmillerphotography@gmail.com>
2022-02-24 20:13:50 +0000

    add new social icons

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
       <Footer/>
       <div className= "social"> 
-        <Link href="https://www.instagram.com/jessc.miller/"><a><Image src='/instagram.ico'  height={22.5} width={22.5}/></a></Link>  <Link href="https://www.twitter.com/jessicacmiller_"><a><Image src='/twitter.ico'  height={22.5} width={22.5}/></a></Link> <Link href="https://www.linkedin.com/in/jessica-cmiller/"><a><Image src='/linkedin.ico'  height={22.5} width={22.5}/></a></Link>
+        <Link href="https://www.instagram.com/jessc.miller/"><a><Image src='/instagram.ico'  height={22.5} width={22.5}/></a></Link>  <Link href="https://www.twitter.com/jessicacmiller_"><a><Image src='/twitter.ico'  height={22.5} width={22.5}/></a></Link> <Link href="https://www.facebook.com/jesscmiller.art"><a><Image src='/facebook (2).ico'  height={22.5} width={22.5}/></a></Link> <Link href="https://www.linkedin.com/in/jessica-cmiller/"><a><Image src='/linkedin.ico'  height={22.5} width={22.5}/></a></Link>
       </div>  
     </>
   );
