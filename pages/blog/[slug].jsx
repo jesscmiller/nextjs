eb6bdfb197c0677f3daf0575905537cc589ead61
jesscmiller <jessicacmillerphotography@gmail.com>
2022-04-28 19:56:43 +0000

    slug

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,3 +1,23 @@
+import { getAllSlugs, getPostData } from "../../lib/posts";
+
 export default function BlogPost() {
-  return <div>This is the blog post page</div>
+    const { postData } = props;
+  return <div>{postData.title}</div>
 }
+
+export const getStaticPaths = () => {
+  const paths = getAllSlugs();
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = ({ params }) => {
+  const postData = getPostData(params.slug);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
